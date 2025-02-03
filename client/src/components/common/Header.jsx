4cbcab5bde00f5fbdd7c620cbd31e4267ad85075
rajesh <rajesh.t7982@gmail.com>
2025-02-03 20:37:04 +0530

    v3

---
+++ client/src/components/common/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div>
+      <nav className='header d-flex justify-content-between align-items-center'>
+        <div className="d-flex justify-content-center">
+          <Link to='/'>
+            LOGO
+          </Link>
+        </div>
+        <ul className="d-flex justify-content-around list-unstyled header-links ">
+          <li>
+            <Link to=''>Home</Link>
+          </li>
+          <li>
+            <Link to='signin'>Signin</Link>
+          </li>
+          <li>
+            <Link to='signup'>Signup</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
