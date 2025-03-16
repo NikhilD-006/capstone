ab578e0d06107f983d059f5b8ac76a559245d14a
nikhild006 <nikhildevarajugattu@gmail.com>
2025-03-16 18:59:09 +0530

    working project

--- server/APIs/userApi.js
+++ server/APIs/userApi.js
@@ -26,5 +26,14 @@ userApp.put('/comment/:articleId',expressAsyncHandler(async(req,res)=>{
     res.status(200).send({message:"comment added",payload:articleWithComments})
 
 }))
+// Get all articles
+userApp.get("/articles", expressAsyncHandler(async (req, res) => {
+    try {
+        const articles = await Article.find(); // Fetch all articles from MongoDB
+        res.status(200).json({ message: "Articles fetched successfully", payload: articles });
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching articles", error: error.message });
+    }
+}));
 
 module.exports=userApp;
